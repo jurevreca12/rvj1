d2d89e6c98304b125f14daf03f3a4943de8e8100
jurevreca12 <jurevreca12@gmail.com>
2020-12-10 12:14:17 +0100

    The isntruction interface is passing basic test.

--- rtl/jedro_1_instr.v
+++ rtl/jedro_1_instr.v
@@ -17,15 +17,21 @@ module jedro_1_instr
 (
 	input					 	 clk_i,
 	input						 rstn_i,
+	input 						 get_next_instr_i,	// A signal that specifys that we can get the next isntruction (controlled by the cores FSM)
+	input						 jmp_instr_i,		// specify that we encountered a jump instruction and the program counter should be changed to jmp_address_i
+	input	   [`DATA_WIDTH-1:0] jmp_address_i,		// The address to jump to, after we had encountered a jump instruction
 	
-	output  					 rsta_o,
-	output reg					 en_o,	
-	output     [`DATA_WIDTH-1:0] addr_o,
-	input 	   [`DATA_WIDTH-1:0] data_i
+	// Interface to the ROM memory
+	output  					 rsta_o,	// Reset the memory
+	output reg					 en_o,		// Enable the memory
+	output     [`DATA_WIDTH-1:0] addr_o,	// The relevant address of the isntruction
+	input 	   [`DATA_WIDTH-1:0] data_i,	// The instruction
+
+	// Interface to the decoder
+	output reg [`DATA_WIDTH-1:0] cinstr_o		// The current instruction (to be decoded)
 );
 
 reg [`DATA_WIDTH-1:0] pc_r;
-reg [`DATA_WIDTH-1:0] cinstr;
 
 // COMBINATIAL LOGIC
 // The output address just follows pc_r
@@ -38,12 +44,30 @@ always @(posedge clk_i) begin
 	if (rstn_i == 1'b0) begin
 		pc_r <= `BOOT_ADDR;
 		en_o <= 1'b1;
+		cinstr_o <= 0;
 	end
-	else begin
-		cinstr <= data_i;
-		pc_r   <= pc_r + 4; // TODO add suport for jump instructions
+	else if (get_next_instr_i == 1'b1) begin
+		if (jmp_instr_i == 1'b1) begin
+			pc_r <= jmp_address_i;
+		end
+		else begin
+			pc_r <= pc_r + 4;
+		end 
 	end
+	
+	cinstr_o <= data_i;
+	
 end
 
+`ifdef COCOTB_SIM
+initial begin
+	$dumpfile ("jedro_1_instr_test.vcd");
+	$dumpvars (0, jedro_1_instr);
+end
+`endif
+
 endmodule
+
+
+
 
--- tb/cocotb/test_instr.py
+++ tb/cocotb/test_instr.py
@@ -1,23 +1,49 @@
 import cocotb
-from cocotb.triggers import Timer, RisingEdge, FallingEdge
+from cocotb.triggers import Timer, RisingEdge, FallingEdge, Edge, First
 from cocotb.clock import Clock
 from cocotb.result import ReturnValue, TestFailure
+from cocotb.utils import get_sim_time
 
 CLK_PERIOD = 1000 # ns or 1 MHz
 
+instr_memory = [0xAA, # 0x0
+		  	    0xBB, # 0x4
+		  	    0xCC, # 0x8
+				0xDD, # 0xC
+				0xEE, # 0x10
+				0xFF, # 0x14
+				0x11] # 0x18
+
 @cocotb.coroutine
 def set_inputs_to_zero(dut):
-	dut.clk_i  <= 0
-	dut.rstn_i <= 0
-	dut.data_i <= 0   
-        yield Timer(0)
+	dut.clk_i  			 <= 0
+	dut.rstn_i 			 <= 1
+	dut.data_i 			 <= 0
+	dut.get_next_instr_i <= 0
+	dut.jmp_instr_i 	 <= 0
+	dut.jmp_address_i 	 <= 0
+	yield Timer(0)
 
 @cocotb.coroutine
 def reset_dut(dut, rstn, duration):
 	rstn <= 0
-	yield Timer(duration)
+	yield Timer(duration, units='ns')
 	rstn <= 1
 	dut._log.info("Reset complete.")
+
+
+# The memory SPROM has a READ latency of 1 clock cycle.
+async def memory(dut):
+	while True:
+		await First(Edge(dut.addr_o), Edge(dut.rstn_i))
+		dut._log.info(f"Memory triggered at time {get_sim_time(units='ns')} ns, reset is {dut.rstn_i}")
+		await Timer(CLK_PERIOD, units='ns')
+		if dut.rstn_i == 1:
+			dut._log.info(f"Memory read at address {int(dut.addr_o)}.")
+			dut.data_i <= instr_memory[int(int(dut.addr_o) / 4)]
+		else:
+			dut.data_i <= 0
+
 
 @cocotb.test()
 def instr_if_read_basic(dut):
@@ -25,32 +51,43 @@ def instr_if_read_basic(dut):
 	
 	yield set_inputs_to_zero(dut)
 	
-	cocotb.fork(Clock(dut.clk_i, CLK_PERIOD/2).start()) 
+	cocotb.fork(Clock(dut.clk_i, CLK_PERIOD, units='ns').start())
+	cocotb.fork(memory(dut))
 	
 	# First reset the block
 	yield reset_dut(dut, dut.rstn_i, 2*CLK_PERIOD) 
 
-        dut.data_i <= 0xAA
+	yield FallingEdge(dut.clk_i)
+	yield RisingEdge(dut.clk_i)	
+	dut.get_next_instr_i <= 1
 
-        if dut.cinstr == 0xAA:
-            raise TestFailure("ERROR 0: Looks like cinstr register is infered as a latch, not a flipflop. \
-                               It shouldn't change before the positive clock edge.")
+	if dut.cinstr_o == 0xAA:
+		raise TestFailure("ERROR 0: Looks like cinstr register is infered as a latch, not a flipflop. \
+	 	                   It shouldn't change before the positive clock edge.")
 
-        yield RisingEdge(dut.clk_i)
+	yield RisingEdge(dut.clk_i)
 
-        if dut.cinstr != 0xAA:
-            raise TestFailure("ERROR 1: The current instruction register cinstr should contian the 0xAA instruction.")
+	yield RisingEdge(dut.clk_i)
+
+	if dut.cinstr_o != 0xAA:
+		raise TestFailure(f"ERROR 1: The current instruction register cinstr should contain the 0xAA instruction.\
+							Instead it is the {dut.cinstr_o} instruction")
+
+	if dut.pc_r != 0x04:
+		raise TestFailure(f"ERROR 2: The current program counter should be 4, not {dut.pc_r}.")
+
+
+	if dut.cinstr_o == 0xBB:
+		raise TestFailure("ERROR 0: Looks like cinstr register is infered as a latch, not a flipflop. \
+						   It shouldn't change before the positive clock edge.")
+
+	yield RisingEdge(dut.clk_i)
+
+	if dut.pc_r != 0x08:
+		raise TestFailure(f"ERROR 2: The current program counter should be 8, not {dut.pc_r}.")
+
+	if dut.cinstr_o != 0xBB:
+		raise TestFailure("ERROR 1: The current instruction register cinstr should contian the 0xAA instruction.")
 	
-        dut.data_i <= 0xBB;
-
-        if dut.cinstr == 0xBB:
-            raise TestFailure("ERROR 0: Looks like cinstr register is infered as a latch, not a flipflop. \
-                               It shouldn't change before the positive clock edge.")
-
-        yield RisingEdge(dut.clk_i)
-
-        if dut.cinstr != 0xBB:
-            raise TestFailure("ERROR 1: The current instruction register cinstr should contian the 0xAA instruction.")
-	
-        yield Timer (5*CLK_PERIOD)
+	yield Timer (5*CLK_PERIOD, units='ns')
 	dut._log.info("Test instr_if_read_basic finnished.")	
