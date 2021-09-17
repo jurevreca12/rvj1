af493829e5e330251cd28d190c5e403fd8e111bf
jurevreca12 <jurevreca12@gmail.com>
2021-09-17 12:23:50 +0200

    Added modports to ram interface.s

--- rtl/inc/interfaces.sv
+++ rtl/inc/interfaces.sv
@@ -1,16 +1,20 @@
 package interfaces;
-	interface if_ram_oneway #(
+	interface if_ram_1way #(
 			parameter ADDR_WIDTH=32,
 			parameter DATA_WIDTH=32
-		);
+		) (input clk, input reset);
 
 		logic		 		 					 ram_req;
 		logic		 		 					 ram_gnt;
+		logic									 ram_rvalid;
 		logic [ADDR_WIDTH-1:0] ram_addr;
 		logic [DATA_WIDTH-1:0] ram_rdata;
+
+		modport MASTER(input ram_gnt, ram_rvalid, ram_rdata, output ram_req, ram_addr)
+		modport SLAVE(output ram_gnt, ram_rvalid, ram_rdata, input  ram_req, ram_addr)
 	endinterface: if_ram_oneway
 
-	interface if_ram_twoway #(
+	interface if_ram_2way_32b_data #(
 		parameter ADDR_WIDTH=32,
 	);
 		
@@ -22,7 +26,13 @@ package interfaces;
 		logic [ADDR_WIDTH-1:0] ram_addr;
 		logic [31:0]					 ram_wdata;
 		logic [31:0]					 ram_rdata;
-		logic									 ram_err;	
+		logic									 ram_err;
+
+		modport MASTER(input  ram_gnt, ram_rvalid, ram_rdata, ram_err,
+									 output	ram_req, ram_we, ram_be, ram_addr, ram_wdata)
+		modport SLAVE (output ram_gnt, ram_rvalid, ram_rdata, ram_err,
+									 input	ram_req, ram_we, ram_be, ram_addr, ram_wdata)
+
 	endinterface: if_ram_twoway
 
 endpackage	
