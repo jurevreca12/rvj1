d9fa2d7afef45818968f79ddf2ffd0336582bc5f
jurevreca12 <jurevreca12@gmail.com>
2021-09-29 23:13:23 +0200

    retabed.

--- rtl/jedro_1_lsu.sv
+++ rtl/jedro_1_lsu.sv
@@ -16,17 +16,17 @@ import jedro_1_defines::*;
 
 module jedro_1_lsu
 (
-	input clk_i,
-	input rstn_i,
-	
-	input lsu_new_ctrl_i, // signals if the control signal is valid (i.e. new instruction)
-	input [LSU_CTRL_WIDTH-1:0] lsu_ctrl_i,
+  input clk_i,
+  input rstn_i,
+  
+  input lsu_new_ctrl_i, // signals if the control signal is valid (i.e. new instruction)
+  input [LSU_CTRL_WIDTH-1:0] lsu_ctrl_i,
 
-	// Decoded signals from the decoder
-	input [REG_ADDR_WIDTH-1:0] lsu_regdest_i,
-	input [REG_ADDR_WIDTH-1:0] lsu_regsrc1_i,
-	input [11:0]				lsu_offset_i,
-	input [REG_ADDR_WIDTH-1:0] lsu_regsrc2_i	
+  // Decoded signals from the decoder
+  input [REG_ADDR_WIDTH-1:0] lsu_regdest_i,
+  input [REG_ADDR_WIDTH-1:0] lsu_regsrc1_i,
+  input [11:0]        lsu_offset_i,
+  input [REG_ADDR_WIDTH-1:0] lsu_regsrc2_i  
 );
 
 
