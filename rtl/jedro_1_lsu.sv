bf63cf5731dcf1af31b46000222cac271f0d1c02
jurevreca12 <jurevreca12@gmail.com>
2022-02-27 10:52:18 +0100

    Fixed store-byte align issues. 39 riscof tests now passing.

--- rtl/jedro_1_lsu.sv
+++ rtl/jedro_1_lsu.sv
@@ -49,9 +49,10 @@ logic [1:0]                byte_addr_r;
 logic                      ram_start_decode_r;
 logic [LSU_CTRL_WIDTH-1:0] ctrl_save_r;
 
+logic [31:0]               active_write_word;
  
 /**************************************
-* WRITE ENABLE SIGNAL
+* WRITE ENABLE SIGNAL / INPUT MUXING
 **************************************/
 logic is_write; // Is the current ctrl input a write
 logic [DATA_WIDTH/8 - 1:0] we; // write enable signal
@@ -59,15 +60,40 @@ logic [DATA_WIDTH/8 - 1:0] we; // write enable signal
 assign is_write = ctrl_i[LSU_CTRL_WIDTH-1];
 
 always_comb begin
+    active_write_word = 0;
     if (is_write == 1'b1) begin
-        if (ctrl_i[LSU_CTRL_WIDTH-2:0] == 3'b010)
-            we = 4'b1111; // word write
-        else if (ctrl_i[LSU_CTRL_WIDTH-2:0] == 3'b001)
-            we = 4'b0011; // half-word write
-        else if (ctrl_i[LSU_CTRL_WIDTH-2:0] == 3'b000)
-            we = 4'b0001; // byte write
-        else
-            we = 4'b0000;
+        if      (ctrl_i == LSU_STORE_BYTE) begin
+            if      (addr_i[1:0] == 2'b00) begin      
+                active_write_word = {24'b0, wdata_i[7:0]};
+                we = 4'b0001;
+            end
+            else if (addr_i[1:0] == 2'b01) begin
+                active_write_word = {16'b0, wdata_i[7:0], 8'b0};
+                we = 4'b0010;
+            end
+            else if (addr_i[1:0] == 2'b10) begin
+                active_write_word = {8'b0, wdata_i[7:0], 16'b0};
+                we = 4'b0100;
+            end
+            else begin
+                active_write_word = {wdata_i[7:0], 24'b0};
+                we = 4'b1000;
+            end
+        end
+        else if (ctrl_i == LSU_STORE_HALF_WORD) begin
+            if (addr_i[1:0] == 2'b00) begin
+                active_write_word = {16'b0, wdata_i[15:0]};
+                we = 4'b0011; 
+            end
+            else begin
+                active_write_word = {wdata_i[15:0], 16'b0};
+                we = 4'b1100; 
+            end
+        end
+        else begin
+            active_write_word = wdata_i;
+            we = 4'b1111;
+        end
     end
     else begin
         we = 4'b0000;
@@ -137,7 +163,7 @@ always_ff @(posedge clk_i) begin
     end
 end
 
-always @(posedge clk_i) begin
+always_ff @(posedge clk_i) begin
     if (rstn_i == 1'b0) begin
         byte_addr_r <= 2'b00;
     end
@@ -174,7 +200,7 @@ always_ff @(posedge clk_i) begin
     else begin
        data_mem_if.addr <= addr_i;
        data_mem_if.we <= we & {4{ctrl_valid_i}};
-       data_mem_if.wdata <= wdata_i; 
+       data_mem_if.wdata <= active_write_word; 
     end
 end
 
