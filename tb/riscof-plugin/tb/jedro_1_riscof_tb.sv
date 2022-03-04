7fcd1933b1a1c4788b1c5aa60bd520ae69ad8d2a
jurevreca12 <jurevreca12@gmail.com>
2022-03-04 16:06:30 +0100

    Fixed problem with testbench, 46 riscof tests now passing.

--- tb/riscof-plugin/tb/jedro_1_riscof_tb.sv
+++ tb/riscof-plugin/tb/jedro_1_riscof_tb.sv
@@ -56,8 +56,8 @@ module jedro_1_riscof_tb();
   end
 
   // get stard and end address of the signature region
-  start_addr = data_mem.ram_memory.RAM[SIG_START_ADDR_CELLNUM][20-1:0];
-  end_addr   = data_mem.ram_memory.RAM[SIG_END_ADDR_CELLNUM][20-1:0];
+  start_addr = data_mem.ram_memory.RAM[SIG_START_ADDR_CELLNUM][$clog2(MEM_SIZE_WORDS*4)-1:0];
+  end_addr   = data_mem.ram_memory.RAM[SIG_END_ADDR_CELLNUM][$clog2(MEM_SIZE_WORDS*4)-1:0];
 
   sig_file = $fopen("dut.signature", "w");
   for (j=start_addr; j < end_addr; j=j+4) begin
