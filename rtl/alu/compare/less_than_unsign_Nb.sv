2e49e6b5f6a3399db03dc7e706e73cd2257d79d1
jvreca <jure.vreca@ijs.si>
2025-10-23 16:20:10 +0000

    removed functions for less than from alu.

--- rtl/alu/compare/less_than_unsign_Nb.sv
+++ rtl/alu/compare/less_than_unsign_Nb.sv
@@ -3,34 +3,21 @@
 //                                                                            //
 //                                                                            //
 //                                                                            //
-// Design Name:    less_than_sign_Nb                                          //
+// Design Name:    less_than_unsign_Nb                                        //
 // Project Name:   riscv-jedro-1                                              //
 // Language:       Verilog                                                    //
 //                                                                            //
 // Description:    The module checks if a is less than b (a < b).             //
-//                 This is for signed (2's complement numbers)                //
+//                 This is for unsigned binary numbers.                       //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-module less_than_sign_Nb #(parameter N = 32) (
-	input  [N-1:0] a,
-	input  [N-1:0] b,
-	output reg [N-1:0] r
+module less_than_unsign_Nb #(parameter int N = 32) (
+  input  logic [N-1:0] a,
+  input  logic [N-1:0] b,
+  output logic [N-1:0] r
 );
-
-wire [N-2:0]  res_abs;
-
-// This comparator module compares all but the signed bit.
-less_than_unsign_Nb #(.N(31)) less_than_unsign_31b_inst (a[N-2:0], b[N-2:0], res_abs);
-
-always@ (*)
-begin
-	case ({a[N-1], b[N-1]})
-		2'b00 	: r = {1'b0, res_abs};
-		2'b10 	: r = 32'b1;
-		2'b01	: r = 32'b0;
-		default : r = {1'b0, res_abs};
-	endcase
-end
-
+  logic lt;
+  assign lt = a < b;
+  assign r = { {N-1{1'b0}}, lt};
 endmodule
