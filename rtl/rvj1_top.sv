9798ea0d972e36927b42c90fbc9996c31c1f8db9
jvreca <jure.vreca@ijs.si>
2026-04-20 20:52:16 +0000

    Updated top port order.

--- rtl/rvj1_top.sv
+++ rtl/rvj1_top.sv
@@ -49,18 +49,18 @@ module rvj1_top import rvj1_pkg::*;
   input  logic              data_rsp_valid_i,
   output logic              data_rsp_ready_o,
 
+  // RISC-V Formal Interface
+  `ifdef RVFI
+  `RVFI_OUTPUTS
+  `endif
+  
   // Interrupt sources
   input logic        irq_external_i,
   input logic        irq_timer_i,
   input logic        irq_sw_i,
   input logic        irq_lcofi_i,
   input logic [15:0] irq_platform_i,
-  input logic        irq_nmi_i,
-
-  // RISC-V Formal Interface
-  `ifdef RVFI
-  `RVFI_OUTPUTS
-  `endif
+  input logic        irq_nmi_i
 );
 
   /****************************************
